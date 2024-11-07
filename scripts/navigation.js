dc6ce080c389781ad2818a3dec5e3b655ecd62ce
LaineyCH <hen20028@byui.edu>
2024-11-07 08:24:43 +0400

    add home page

---
+++ scripts/navigation.js
@@ -0,0 +1,43 @@
+//select HTML elements and assign to constants
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+// select menu buttons
+const homeButton = document.querySelector('#home-button');
+const chamButton = document.querySelector('#cham-button');
+const gitButton = document.querySelector('#git-button');
+const linkButton = document.querySelector('#link-button');
+
+//create event listener for the hamburger menu button
+//toggle the open class on/off for the nav menu and button, when the button is clicked
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    changeActiveNav("home");
+}, false);
+
+function changeActiveNav(activePhrase) {
+    // change active class on nav buttons
+    homeButton.classList.remove('active');
+    chamButton.classList.remove('active');
+    gitButton.classList.remove('active');
+    linkButton.classList.remove('active');
+
+    switch (activePhrase) {
+        case "home":
+            homeButton.classList.add('active');
+            break;
+        case "cham":
+            chamButton.classList.add('active');
+            break;
+        case "git":
+            gitButton.classList.add('active');
+            break;
+        case "link":
+            linkButton.classList.add('active');
+            break;
+        default:
+    }
+}
