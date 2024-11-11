0c026e567f4228124746416c2a4a45d0ac7db844
LaineyCH <hen20028@byui.edu>
2024-11-11 20:23:30 +0400

    add table view and adjust styling

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,6 @@
 const cards = document.querySelector('#cards');
+const table = document.querySelector('#table-view');
+let members = []
 
 async function getMemberData() {
     try {
@@ -6,8 +8,17 @@ async function getMemberData() {
         if (!response.ok) {
             throw new Error('Could not fetch member data');
         }
-        const data = await response.json();
-        displayMemberCards(data)
+        members = await response.json();
+        members.sort((a, b) => {
+            if (a.companyname < b.companyname) {
+                return -1;
+            }
+            if (a.companyname > b.companyname) {
+                return 1;
+            }
+            return 0;
+        });
+        displayMemberCards()
     } catch (error) {
         console.error("Encountered error during fetch:", error);
     }
@@ -15,7 +26,9 @@ async function getMemberData() {
 
 getMemberData();
 
-function displayMemberCards(members) {
+function displayMemberCards() {
+    cards.innerHTML = '';
+    table.innerHTML = '';
     members.forEach((member) => {
         // create html elements
         const card = document.createElement('section');
@@ -42,3 +55,45 @@ function displayMemberCards(members) {
         cards.appendChild(card);
     })
 }
+
+function displayMemberTable() {
+    cards.innerHTML = '';
+    table.innerHTML = '';
+    members.forEach(member => {
+        const tableRow = document.createElement('tr');
+        const companyTd = document.createElement('td');
+        const ownerTd  = document.createElement('td');
+        const addressTd = document.createElement('td');
+        const phoneTd = document.createElement('td');
+        const urlTd = document.createElement('td');
+        const url = document.createElement('a');
+        companyTd.classList.add('bold');
+        companyTd.textContent = member.companyname;
+        ownerTd.textContent = member.ownername
+        addressTd.textContent = `${member.address.number} ${member.address.street}, ${member.address.area}, ${member.address.city}`;
+        phoneTd.textContent = member.phone;
+        url.classList.add('url-table');
+        url.href = `http://${member.url}`;
+        url.textContent = member.url;
+        url.target = '_blank';
+        urlTd.appendChild(url);
+        tableRow.appendChild(companyTd);
+        tableRow.appendChild(ownerTd);
+        tableRow.appendChild(addressTd);
+        tableRow.appendChild(phoneTd);
+        tableRow.appendChild(urlTd);
+        table.appendChild(tableRow);
+    });
+}
+
+// select display choice buttons
+const gridButton = document.querySelector('#grid-button');
+const tableButton = document.querySelector('#table-button');
+
+// create event listeners for display choice
+gridButton.addEventListener('click', () => {
+    displayMemberCards();
+});
+tableButton.addEventListener('click', () => {
+    displayMemberTable();
+});
