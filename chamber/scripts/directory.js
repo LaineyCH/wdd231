93f595647b9fbccfbd218105467c5e3641572a13
LaineyCH <hen20028@byui.edu>
2024-11-11 20:56:29 +0400

    add business owner details

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -33,26 +33,43 @@ function displayMemberCards() {
         // create html elements
         const card = document.createElement('section');
         const companyName = document.createElement('h2');
-        const ownerName = document.createElement('h3');
         const image = document.createElement('img');
+        const detailsDiv = document.createElement('div');
+        const ownerName  = document.createElement('p');
+        const address = document.createElement('p');
+        const phone = document.createElement('p');
+        const urlP = document.createElement('p');
+        const url = document.createElement('a');
 
         // set content / attributes
         companyName.textContent = `${member.companyname}`;
-        ownerName.textContent = `${member.ownername}`;
         card.setAttribute('class', 'card');
         image.setAttribute('src', `./images/${member.img}`);
         image.setAttribute('alt', `${member.companyname} Business Logo`);
+        ownerName.classList.add('bold');
+        ownerName.textContent = member.ownername
+        address.textContent = `${member.address.number} ${member.address.street}, ${member.address.area}, ${member.address.city}`;
+        phone.textContent = member.phone;
+        url.href = `http://${member.url}`;
+        url.textContent = member.url;
+        url.target = '_blank';
         // Only add lazy loading for images after the first one
         if (index !== 0) {
             image.setAttribute('loading', 'lazy');
         }
         image.setAttribute('width', '500');
         image.setAttribute('height', '500');
+        detailsDiv.setAttribute('class', 'details-div');
 
         // add name and img to card
         card.appendChild(companyName);
-        card.appendChild(ownerName);
         card.appendChild(image);
+        detailsDiv.appendChild(ownerName);
+        detailsDiv.appendChild(address);
+        detailsDiv.appendChild(phone);
+        urlP.appendChild(url)
+        detailsDiv.appendChild(urlP);
+        card.appendChild(detailsDiv);
 
         // add card to cards element in html
         cards.appendChild(card);
